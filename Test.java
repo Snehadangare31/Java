86142c3a79edf7e39df3fc02d70c480562a7ce1f
Sneha Dangare <163710095+Snehadangare31@users.noreply.github.com>
2025-02-28 19:43:54 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,32 +1,20 @@
-// Java program to demonstrate extracting first
-// letter of each word using Regex
+// A Java program with overloaded main() 
+import java.io.*; 
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+public class Test { 
+	
+	// Normal main() 
+	public static void main(String[] args) { 
+		System.out.println("Hi Geek (from main)"); 
+		Test.main("Geek"); 
+	} 
 
-public class Test 
-{
-	public static void main(String[] args) 
-	{
-		String s1 = "subject";
-		String s2 = "A Computer Science ";
-		
-		Pattern p = Pattern.compile("\\b[a-zA-Z]");
-		
-		Matcher m1 = p.matcher(s1);
-		Matcher m2 = p.matcher(s2);
-		
-		System.out.println("First letter of each word from string \"" + s1 + "\" : ");
-		while (m1.find())
-			System.out.print(m1.group());
-		
-		System.out.println();
-		
-		System.out.println("First letter of each word from string \"" + s2 + "\" : ");
-		
-		while (m2.find()) 
-			System.out.print(m2.group());
-		
-		
-	}
-}
+	// Overloaded main methods 
+	public static void main(String arg1) { 
+		System.out.println("Hi, " + arg1); 
+		Test.main("Dear Geek","My Geek"); 
+	} 
+	public static void main(String arg1, String arg2) { 
+		System.out.println("Hi, " + arg1 + ", " + arg2); 
+	} 
+} 
